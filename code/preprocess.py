a836291a1bed165656e59d7a9eccf0c71622f9cf
Reid Arwood <reidarwood@gmail.com>
2020-11-17 17:04:20 -0500

    added virtualenv

---
+++ code/preprocess.py
@@ -0,0 +1,10 @@
+
+
+def get_data(filename):
+    """
+    Reads in a path to a csv file and returns a pandas dataframe
+    
+    :param filename: path to the file
+    :return: pandas dataframe of the file
+    """
+    
