a30b87c3d7cdc6957565a679f96b2784f19004c6
Reid Arwood <reidarwood@gmail.com>
2020-11-22 19:00:32 -0500

    added alpha vantage

--- code/preprocess.py
+++ code/preprocess.py
@@ -1,4 +1,15 @@
 import pandas
+from alpha_vantage.timeseries import TimeSeries
+
+api_key = 'A759HSE0DRDPALHC'
+path_to_data = "../data/"
+ts = TimeSeries(key=api_key, output_format='pandas')
+def install_data(stock_ticker):
+    file_path = path_to_data + stock_ticker + ".csv"
+    data, meta_data = ts.get_daily(symbol=stock_ticker, outputsize='full')
+    print(meta_data)
+    data.to_csv(file_path)
+    
 
 def get_data(filename):
     """
@@ -8,3 +19,5 @@ def get_data(filename):
     :return: pandas dataframe of the file
     """
     return pandas.read_csv(filename)
+
+install_data("AAPL")
